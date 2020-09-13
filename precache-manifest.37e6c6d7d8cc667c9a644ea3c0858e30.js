0db28d6c7376db992915b8db0f3e135e355d3c23
Sergio C <sercalgo1992@gmail.com>
2020-09-13 19:31:17 +0200

    Updates

---
+++ precache-manifest.37e6c6d7d8cc667c9a644ea3c0858e30.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf65240f44a5409cc169b39340e05427",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3feb2616ff209c5f2e5c",
+    "url": "/static/css/main.02dcc331.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3feb2616ff209c5f2e5c",
+    "url": "/static/js/main.091b3886.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
